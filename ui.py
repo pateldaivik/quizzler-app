06df1cdae6cf80fe7bca35e86f59dcaeade4fe1b
Daivik <patel.daivik8@gmail.com>
2021-03-29 12:29:35 -0400

    Final commit

---
+++ ui.py
@@ -0,0 +1,52 @@
+THEME_COLOR = "#375362"
+from tkinter import *
+from quiz_brain import QuizBrain
+
+class QuizInteface:
+    def __init__(self,quiz_brain:QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title('Quizzler')
+        self.window.config(padx=20,pady=20,bg=THEME_COLOR)
+        self.score = Label(text='Score: 0',fg='white',bg=THEME_COLOR)
+        self.score.grid(row=0,column=1)
+        self.canvas = Canvas(width=300,height=250,bg='white')
+        self.q_text = self.canvas.create_text(150,125,width=280,text='Some text',fill=THEME_COLOR,font=('Arial',20,'italic'))
+        self.canvas.grid(row=1,column=0,columnspan=2,pady=50)
+        t_image = PhotoImage(file = 'images/true.png')
+        f_image = PhotoImage(file='images/false.png')
+        self.true_btn = Button(image = t_image,highlightthickness=0,command = self.true_pressed)
+        self.true_btn.grid(row=2,column=0)
+        self.false_btn = Button(image=f_image, highlightthickness=0,command = self.false_pressed)
+        self.false_btn.grid(row=2, column=1)
+        self.get_next()
+
+        self.window.mainloop()
+
+    def get_next(self):
+        self.canvas.config(bg='white')
+        if self.quiz.still_has_questions():
+           self.score.config(text=f"Score: {self.quiz.score}")
+           q_text = self.quiz.next_question()
+           self.canvas.itemconfig(self.q_text,text = q_text)
+        else:
+            self.canvas.itemconfig(self.q_text, text='You have reached the end of quiz.')
+            self.true_btn.config(state='disabled')
+            self.false_btn.config(state='disabled')
+
+    def true_pressed(self):
+      is_right  = self.quiz.check_answer('True')
+      self.give_feedback(is_right)
+
+    def false_pressed(self):
+      is_right = self.quiz.check_answer('False')
+      self.give_feedback(is_right)
+
+    def give_feedback(self,is_right):
+        if is_right:
+            self.canvas.config(bg='green')
+        else:
+            self.canvas.config(bg='red')
+
+        self.window.after(1000,self.get_next)
+
